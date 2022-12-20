e12d124a7fecd0ba4036b708fcaa4b37ae15f289
Basicdots <basicdots@Basicdotss-MacBook-Pro.local>
2022-12-20 06:58:06 -0800

    First Commit

---
+++ android/app/src/main/kotlin/co/basicdots/onboarding_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.basicdots.onboarding_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/onboarding_contents.dart
@@ -0,0 +1,32 @@
+class OnboardingContents {
+  final String title;
+  final String image;
+  final String desc;
+
+  OnboardingContents({
+    required this.title,
+    required this.image,
+    required this.desc,
+  });
+}
+
+List<OnboardingContents> contents = [
+  OnboardingContents(
+    title: "Fast and Easy",
+    image: "assets/images/onboardingellipse1.png",
+    // image: "assets/images/image1.png",
+    desc: "Deltax provides a faster and easy method of tax and levy payment for Delta State people",
+  ),
+  OnboardingContents(
+    title: "Secured",
+    image: "assets/images/ellipse2.png",
+    desc:
+        "Taxes and levies paid through the Deltax platform is safe, secured and free of irregularities",
+  ),
+  OnboardingContents(
+    title: "Accessible",
+    image: "assets/images/ellipse3.png",
+    desc:
+        "Deltax provides access to tax and levy records, there by making the process transparent",
+  ),
+];
